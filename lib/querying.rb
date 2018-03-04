12fac71cedefc263bec7c9472df1f53dfc6963b8
amanda johnson <manda.panda91@yahoo.com>
2018-03-04 20:59:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,6 +26,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.id
   WHERE characters.species = 'human'
+  GROUP BY(series.title)
   ORDER BY(characters.species) DESC LIMIT 1;"
 end
 
