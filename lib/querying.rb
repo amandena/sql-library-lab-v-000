a34fee13879856e6e53faebf6c1abef351ae5082
amanda johnson <manda.panda91@yahoo.com>
2018-03-04 16:32:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  JOIN characters
+  ON series.id = characters.id
+  GROUP BY(characters.species)
+  ORDER BY(characters.species) DESC LIMIT 1 
+  WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
